830522ea1b38c9b33d05dfd1977e582919dae3af
Andrés Ussa Caycedo <andresussa@gmail.com>
2016-05-05 13:20:31 +0100

    Add Linux files' paths and epochs export.

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 clear all
-EEG = pop_biosig('H:\thesis\2.1.1min');
+EEG = pop_biosig('/home/andres/repos/eeg_hei_soton/2.1.1min');
 EEG.setname = '1MIN data';
-% EEG = pop_chanedit(EEG, 'lookup','/home/andres/MATLAB/eeglab13_5_4b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp');
-EEG=pop_chanedit(EEG, 'lookup','H:\\thesis\\eeglab13_5_4b\\plugins\\dipfit2.3\\standard_BESA\\standard-10-5-cap385.elp');
+EEG = pop_chanedit(EEG, 'lookup','/home/andres/MATLAB/eeglab13_5_4b/plugins/dipfit2.3/standard_BESA/standard-10-5-cap385.elp');
+% EEG=pop_chanedit(EEG, 'lookup','H:\\thesis\\eeglab13_5_4b\\plugins\\dipfit2.3\\standard_BESA\\standard-10-5-cap385.elp');
 EEG = pop_select( EEG,'nochannel',{'FPZ' 'AUX1' 'AUX2' 'AUX3' 'AUX4' 'AUX5' 'AUX6' 'AUX7' 'AUX8' 'PG1' 'PG2' 'A1' 'A2'});
 notchlo = 49;
 notchhi = 51;
@@ -14,7 +14,15 @@ EEG = pop_eegfiltnew(EEG, bandlo, bandhi, 3380, 0, [], 0);
 EEG.data(1,1:EEG.srate*epochlen:EEG.pnts) = 1;
 EEG = pop_chanevent(EEG, 1,'edge','leading','edgelen',0,'duration','on');
 EEG = pop_epoch(EEG, {  }, [-1  2], 'newname', '1MIN data epochs', 'epochinfo', 'yes');
-EEG = pop_rmbase(EEG, [-1000     0]);
+EEG = pop_rmbase(EEG, [-1000     0]); % Check what Baseline removing is for and proper Value!!!!
 [EEG, Indexes] = pop_eegthresh(EEG,1,[1:19] ,-100,100,-1,1.998,0,0);
 EEG = pop_rejepoch( EEG, Indexes, 0);
-% eeglab redraw
+
+for i = 1:EEG.trials
+    name = ['epochs/e', num2str(i),'.mat'];
+    var = EEG.data(:,:,i);
+%     name = 'epochs.mat';
+%     var = EEG.data;
+    save(name,'var');
+end
+eeglab redraw
