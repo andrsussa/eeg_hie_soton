c4a1ab678004b1687727244005c0260cf50f0f08
Andrés Ussa Caycedo <andresussa@gmail.com>
2016-07-04 16:27:49 +0100

    Modify brain connectivity plotting script to a function.

--- ploteegrel.m
+++ ploteegrel.m
@@ -1,13 +1,14 @@
-clear
-load('/home/andres/repos/eeg_hei_soton/HERMES output/eeg_hei.mat')
-M = cell2mat(indexes.PLV.data);
-n = size(M,1);
-L = num2cell(1:n);
-imagesc(M(:,:,1));
-set(gca, 'XTick', 1:n);
-set(gca, 'YTick', 1:n);
-set(gca, 'XTickLabel', L);
-set(gca, 'YTickLabel', L);
-title('COH', 'FontSize', 14);
-colormap('jet');
-colorbar;
+function ploteegrel( M )
+
+    n = size(M,1);
+    L = num2cell(1:n);
+    imagesc(M(:,:,1));
+    set(gca, 'XTick', 1:n);
+    set(gca, 'YTick', 1:n);
+    set(gca, 'XTickLabel', L);
+    set(gca, 'YTickLabel', L);
+    title('COH', 'FontSize', 14);
+    colormap('jet');
+    colorbar;
+
+end
