ca144bf22bc1e1250570dd189f5398913df752d7
Andres Ussa <au5n14@ecs.soton.ac.uk>
2016-07-26 20:20:03 +0100

    Add option to test RUNICA vs FASTICA.

--- testICA.m
+++ testICA.m
@@ -6,14 +6,30 @@ t = 0:1/100:100;
 x1 = 2*sin(0.2*pi*t);
 x2 = 2*sawtooth(0.1*pi*t,1);
 
-x3 = [x1; x2];
+fi = 1;
+figure(fi), plot(t, x1, t, x2);
 
-plot(t, x1, t, x2);
+x3 = x1 - 2*x2;
+x4 = 1.73*x1 +3.41*x2;
 
-[mixSingal,w,wi,rc] = instamix( [x1;x2], .01);
+fi = fi + 1;
+figure(fi), subplot(2,1,1), plot(t, x3);
+figure(fi), subplot(2,1,2), plot(t, x4);
 
-figure, plot(t, mixSingal);
+mixSignal = [x3; x4];
+% [mixSignal,~,~,~] = instamix( [x1;x2], .01);
 
-[icasig, A, W] = fastica(mixSingal);
+fi = fi + 1;
+figure(fi), plot(t, mixSignal);
 
-figure, plot(t, icasig);
+[weights,sphere] = runica(mixSignal);
+
+runicaSig = weights \ mixSignal;
+
+fi = fi + 1;
+figure(fi), plot(t, runicaSig);
+
+[icasig, A, W] = fastica(mixSignal);
+
+fi = fi + 1;
+figure(fi), plot(t, icasig);
